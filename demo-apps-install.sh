75dc549943b29714749beb6f8c0248caa191278c
Oren Penso <openso@openso-a01.vmware.com>
2021-10-14 18:49:15 +0100

    duckypad

--- demo-apps-install.sh
+++ demo-apps-install.sh
@@ -1,8 +1,8 @@
 echo "Install ACME-Fit, Sock-Shop and Boutique apps on Kubernetes cluster"
 kubectl create ns acme-fe
 kubectl create ns acme-be
-kubectl create -f acme/acme-fe -n acme-fe
-kubectl create -f acme/acme-be -n acme-be
+kubectl create -f acme-fe-be/acme-fe -n acme-fe
+kubectl create -f acme-fe-be/acme-be -n acme-be
 kubectl create ns boutique
 kubectl create -f boutique/release/kubernetes-manifests.yaml -n boutique
 kubectl create -f sock-shop/deploy/kubernetes/complete-demo.yaml -n sock-shop
